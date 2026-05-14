c4d47881f451de39ad40b6e8df450baa1a1127c0
TkChipChipLeader89 <jonathansotelo99@gmail.com>
2026-05-14 18:28:16 +0000

    Fixed Syntax Errors for mini 3

--- week7-exercises/mini03-race-condition-synchronization/src/main/java/edu/norcocollege/cis18b/week7/mini03/RaceConditionDemo.java
+++ week7-exercises/mini03-race-condition-synchronization/src/main/java/edu/norcocollege/cis18b/week7/mini03/RaceConditionDemo.java
@@ -10,13 +10,17 @@ public class RaceConditionDemo {
         int unsafe = RaceConditionHarness.runUnsafeTrial(threadCount, incrementsPerThread);
         int safe = RaceConditionHarness.runSynchronizedTrial(threadCount, incrementsPerThread);
         int atomic = RaceConditionHarness.runAtomicTrial(threadCount, incrementsPerThread);
+        long syncStart = System.nanoTime();
+        long syncEnd = System.nanoTime();
+        long atomicStart = System.nanoTime();
+        long atomicEnd = System.nanoTime();
 
         System.out.println("Expected count: " + expected);
         System.out.println("Unsafe count: " + unsafe);
         System.out.println("Synchronized count: " + safe);
         System.out.println("Atomic count: " + atomic);
 
-        System.out.println("Sychronized time: " + (syncEnd - syncStart));
-        System.out.println("Atomic Time: " + (atomicEnd -atomicStart));
+        System.out.println("Synchronized time: " + (syncEnd - syncStart));
+        System.out.println("Atomic Time: " + (atomicEnd - atomicStart));
     }
 }
